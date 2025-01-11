9f4ba6b0568ad17e2dd7a4fc1c01d58c7fa00b92
Rashin2002 <rashinprashastha2002@gmail.com>
2025-01-11 18:44:52 +0530

    First 3 interface designs
    Login, Signup pages

--- app/src/main/java/com/example/shoplink/LogInPage.java
+++ app/src/main/java/com/example/shoplink/LogInPage.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class loginpage extends AppCompatActivity {
+public class LogInPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_loginpage);
+        setContentView(R.layout.activity_log_in_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
